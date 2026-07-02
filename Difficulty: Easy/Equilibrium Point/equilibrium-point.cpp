383ab58029753e0ca43c11050361aa828a5fab82
Ankita Maji <137888030+Ankita7033@users.noreply.github.com>
2026-07-03 00:04:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findEquilibrium(vector<int> &arr) {
+
+        long long totalSum = 0;
+
+        for (int x : arr)
+            totalSum += x;
+
+        long long leftSum = 0;
+
+        for (int i = 0; i < arr.size(); i++) {
+
+            totalSum -= arr[i];   
+
+            if (leftSum == totalSum)
+                return i;
+
+            leftSum += arr[i];
+        }
+
+        return -1;
+    }
+};
