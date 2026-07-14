c1df991938b13279c2bf9d084a75a408422d5d5e
Ankita Maji <137888030+Ankita7033@users.noreply.github.com>
2026-07-14 15:34:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge 2 Sorted Linked Lists in Reverse Order/merge-2-sorted-linked-lists-in-reverse-order.cpp
@@ -0,0 +1,61 @@
+/* Structure of a linked list Node
+class Node {
+  public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};*/
+
+class Solution {
+  public:
+    Node *mergeResult(Node *head1, Node *head2) {
+        // code here
+        vector<int>res;
+        
+        
+        if(head1==nullptr) return head2;
+        if(head2 == nullptr) return head1;
+        
+        while(head1 != nullptr && head2!=nullptr){
+            if(head1->data < head2->data){
+                res.push_back(head1->data);
+                head1=head1->next;
+            }
+            else if(head1->data > head2->data){
+                res.push_back(head2->data);
+                head2=head2->next;
+            }
+            else{
+                res.push_back(head1->data);
+                res.push_back(head2->data);
+                head1=head1->next;
+                head2=head2->next;
+            }
+            
+        }
+        
+        while(head1!=nullptr){
+            res.push_back(head1->data);
+            head1= head1->next;
+        }
+        while(head2!=nullptr){
+            res.push_back(head2->data);
+            head2= head2->next;
+        }
+        
+        
+        
+        Node* temp = new Node(0);
+        Node* curr = temp;
+        for(int i=res.size()-1;i>=0;i--) {
+            curr->next = new Node(res[i]);
+            curr = curr->next;
+            
+        }
+        
+        return temp->next;
+    }
+};
