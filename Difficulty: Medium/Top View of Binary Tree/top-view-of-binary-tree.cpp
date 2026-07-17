1b2fdb33adb0d46b2491b472bf845912d32c1e48
Ankita Maji <137888030+Ankita7033@users.noreply.github.com>
2026-07-17 15:03:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -0,0 +1,52 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> topView(Node *root) {
+        // code here
+        vector<int> ans;
+        if(root==nullptr){
+            return ans;
+        }
+        map<int,int>mp;
+        queue<pair<Node*, int>>q;
+        q.push({root,0});
+        while(!q.empty()){
+            auto curr = q.front();
+            q.pop();
+            Node* node = curr.first;
+            int hd = curr.second;
+            
+            if(mp.find(hd)==mp.end()){
+                mp[hd]=node->data;
+            }
+            if(node->left){
+                q.push({node->left,hd-1});
+            }
+            if(node->right){
+                q.push({node->right,hd+1});
+            }
+            
+            
+        }
+        
+        for(auto it : mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+        
+    }
+};
