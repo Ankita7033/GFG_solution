c5b9c0cd708e8c227873817f26fa1ab2e63a3456
Ankita Maji <137888030+Ankita7033@users.noreply.github.com>
2026-08-08 00:34:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairs with Less Than K Diff/pairs-with-less-than-k-diff.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int countPairs(vector<int>& arr, int k) {
+        sort(arr.begin(), arr.end());
+
+        int n = arr.size();
+        int ans = 0;
+        int j = 1;
+
+        for (int i = 0; i < n; i++) {
+            while (j < n && arr[j] - arr[i] < k)
+                j++;
+
+            ans += (j - i - 1);
+
+            if (j == i + 1)
+                j++;
+        }
+
+        return ans;
+    }
+};
