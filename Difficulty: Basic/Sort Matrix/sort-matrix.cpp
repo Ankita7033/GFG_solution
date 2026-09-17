44189c6091bf0f6483fdaca5146177cd8c02579d
Ankita Maji <137888030+Ankita7033@users.noreply.github.com>
2026-09-17 23:38:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort Matrix/sort-matrix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    vector<vector<int>> sortedMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+
+        vector<int> v;
+
+        // Store all elements
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                v.push_back(mat[i][j]);
+            }
+        }
+
+        // Sort all elements
+        sort(v.begin(), v.end());
+
+        // Put them back into matrix
+        int k = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                mat[i][j] = v[k++];
+            }
+        }
+
+        return mat;
+    }
+};
